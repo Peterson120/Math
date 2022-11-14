69c742062e2e4d0aa363aaa9c377f66414a47fdd
Peterson Guo <petersonguo@gmail.com>
2022-11-13 19:36:43 -0500

    Update Linear Regression

--- LinearRegression/LinearRegression.cpp
+++ LinearRegression/LinearRegression.cpp
@@ -21,15 +21,15 @@ bool readData(ld &avgx, ld &avgy) {
 	std::string line;
 	while (1) {
 		fs >> line;
-		if (line[0] < '0' || line[0] > '9') fs.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		if ((line[0] < '0' || line[0] > '9') && line[0] != '-') fs.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 		else break;
 	}
 	t = std::stold(line);
 	do {avgx += t; scatter_x.push_back(t); i++;} while (fs.get() != '\n' && fs >> t);
-	avgx /= i;
+	avgx /= scatter_x.size();
 	while (1) {
 		fs >> line;
-		if (line[0] < '0' || line[0] > '9') fs.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		if ((line[0] < '0' || line[0] > '9') && line[0] != '-') fs.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 		else break;
 	}
 	i = 0;
@@ -39,35 +39,48 @@ bool readData(ld &avgx, ld &avgy) {
 		map[scatter_x[i++]].push_back(t);
 		scatter_y.push_back(t);
 	} while (fs >> t);
-	avgy /= i;
+	avgy /= scatter_y.size();
 	return scatter_x.size() == scatter_y.size();
 }
 
 int main() {
-	std::cin.tie(NULL); std::cout.tie(NULL); fflush(stdout);
+	std::cin.tie(NULL); std::cout.tie(NULL);
+
 	ld avgx = 0, avgy = 0;
 	if (!readData(avgx, avgy)) {std::cout << "Inequivalent # of Points\n"; return 1;}
 	plt::scatter(scatter_x, scatter_y, 10.0, {{"c", "b"}});
-	ld mTop = 0, mBottom = 0;
-	for (auto it = map.begin(); it != map.end(); ++it) { // Linear Regression
-		ld currX = it->x-avgx;
+
+	ld sx = 0, sy = 0, sxy = 0;
+	for (auto it = map.begin(); it != map.end(); it++) { // Linear Regression
 		for (ld i : it->y) {
-			mTop += currX*(i-avgy);
-			mBottom += currX*currX;
+			ld currX = it->x - avgx;
+			sx += currX * currX;
+			ld currY = i - avgy;
+			sy += currY * currY;
+			sxy += currX * currY;
 		}
 	}
-	ld m = mTop/mBottom, b = avgy - m*avgx;
+
+	ld m = sxy / sx, b = avgy - m * avgx;
+	sxy = sxy / (scatter_x.size()-1);
+	sx = sqrt(sx / (scatter_x.size()-1));
+	sy = sqrt(sy / (scatter_y.size()-1));
+	ld r = sxy / (sx * sy);
+
 	std::ofstream fs("out.txt", std::ios::app);
-	fs << "y = " << m << (b > 0 ? "x + " : b == 0 ? "" : "x - ") << std::abs(b) << '\n';
+	fs << "y = " << (m == 1 ? "" : std::to_string(m)) << (b > 0 ? "x + " + std::to_string(abs(b)) : b == 0 ? "x" : ("x - " + std::to_string(abs(b)))) << "\nStrength: " << r << '\n';
+	std::cout << "Accuracy: " << r << '\n';
+
 	std::sort(scatter_x.begin(), scatter_x.end());
 	std::sort(scatter_y.begin(), scatter_y.end());
-	for (long long i = -scatter_x[0] * 8; i <= scatter_x[scatter_x.size()-1] * 8; i++) {
+	for (long long i = -abs(scatter_x[0]) * 8; i <= abs(scatter_x[scatter_x.size()-1]) * 8; i++) {
 		xs.push_back(i);
 		ys.push_back(m*i+b);
 	}
-	plt::named_plot("y = " + std::to_string(m) + (b > 0 ? "x + " : b == 0 ? "" : "x - ") + std::to_string(std::abs(b)), xs, ys);
-	plt::ylim(scatter_x[0] - scatter_x[0] * 1.5, scatter_x[scatter_x.size()-1]*2);
-	plt::xlim(scatter_y[0] - scatter_y[0] * 1.5, scatter_y[scatter_y.size()-1]*2);
+
+	plt::named_plot("y = " + (m == 1 ? "" : std::to_string(m)) + (b > 0 ? "x + " + std::to_string(abs(b)) : b == 0 ? "x" : ("x - " + std::to_string(abs(b)))), xs, ys);
+	plt::xlim(scatter_x[0] - scatter_x[0] * 1.5, scatter_x[scatter_x.size()-1] + scatter_x[scatter_x.size()-1] * 1.5);
+	plt::ylim(scatter_y[0] - scatter_y[0] * 1.5, scatter_y[scatter_y.size()-1] + scatter_y[scatter_y.size()-1] * 1.5);
 	plt::grid(1);
 	plt::legend();
 	plt::show();
--- LinearRegression/run.sh
+++ LinearRegression/run.sh
@@ -1,1 +1,1 @@
-g++ $1 -std=c++11 -I/usr/include/python3.10 -lpython3.10 -o $(echo "$1" | cut -f 1 -d '.') && ./$(echo "$1" | cut -f 1 -d '.')
+g++ $1 -std=c++11 -I/usr/include/python3.10 -lpython3.10 -g -o $(echo "$1" | cut -f 1 -d '.') && ./$(echo "$1" | cut -f 1 -d '.')
